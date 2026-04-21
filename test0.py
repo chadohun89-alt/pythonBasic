ce2fc868c5052917788c6e8d356b056408d553bb
chadohun89-alt <chadohun89@gmail.com>
2026-04-21 12:21:01 +0900

    깃허브 처음 연결

---
+++ test0.py
@@ -0,0 +1,35 @@
+print("와우")
+num = 10
+num2 = 30
+result = num + num2
+print(result)
+name = "이순신"
+print(name)
+job="군인"
+print(name+"은", job+"이다")
+print("배고파" * 5)
+반지름 = 5
+pi = 3.14
+print(반지름*2*pi)
+is_student = True
+is_food = False
+print(is_student)
+print(type(pi))
+# num = input("정수입력 : " )
+# num = int(num)
+# print(num + 10)
+# 한 줄 주석이다
+""" 여러줄 주석이다 """
+# 문제. 너비와 높이를 입력받아 사각형의 넓이를 출력하세요.
+
+# width = input("너비를 입력 : ")
+# width = int(width)
+# height = input("높이를 입력 : ")
+# height = int(height)
+# n3 = width*height
+# print(n3)
+
+width = int(input("너비 입력: "))
+height = int(input("높이 입력"))
+print("넓이:", str(width*height))
+
